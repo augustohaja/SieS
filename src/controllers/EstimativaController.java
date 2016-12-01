59e8b6ab385970626c34b4a3556841b864d34c4b
Mateus Krejci <mateus.krejci@live.com>
2016-11-30 22:27:09 -0200

    Finalizando por hoje

--- src/controllers/EstimativaController.java
+++ src/controllers/EstimativaController.java
@@ -1,5 +1,35 @@
 package controllers;
 
-public class EstimativaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import models.Estimativa;
+
+public class EstimativaController implements Initializable {
+
+	@FXML
+	private AnchorPane aPane;
+	@FXML
+	private Button btnInsere;
+	@FXML
+	private Button btnVerEstimativa;
+	@FXML
+	private Button btnSair;
+	@FXML
+	private Button btnDeleta;
+	
+	private ObservableList<Estimativa> materialObservableList;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+	
+	
 
 }
